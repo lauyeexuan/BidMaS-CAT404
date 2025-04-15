dde474c9e4f9c5d79b270f6d02b85eb2b56f8163
lyxuan <lauyx2002@gmail.com>
2025-04-15 21:14:40 +0800

    stronger username effect

--- src/components/AppLayout.vue
+++ src/components/AppLayout.vue
@@ -29,7 +29,9 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-gray-600" viewBox="0 0 24 24" fill="none" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <span>{{ currentUser?.name || 'User' }}</span>
+            <span class="font-bold text-lg bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
+              {{ currentUser?.name || 'User' }}
+            </span>
           </div>
         </div>
       </div>
